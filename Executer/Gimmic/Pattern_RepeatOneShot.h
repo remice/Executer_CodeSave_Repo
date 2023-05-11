8a9a4f33a6055f94fde0326d268be1ce7c390af4
qortmdwns3 <qortmdwns3@gmail.com>
2023-05-11 20:55:14 +0900

    Setup new bullet pattern
    Add >>>
    Pattern_RepeatOneShot >> Spawn one bullet at all arrows, between delay
    Projectile_DirectToPlayer >> Bullet shot toward player

---
+++ Executer/Gimmic/Pattern_RepeatOneShot.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Gimmic/PatternBase.h"
+#include "Pattern_RepeatOneShot.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EXECUTER_API APattern_RepeatOneShot : public APatternBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Executer/Gimmic/Projectile_DirectToPlayer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Gimmic/Projectile.h"
+#include "Projectile_DirectToPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EXECUTER_API AProjectile_DirectToPlayer : public AProjectile
+{
+	GENERATED_BODY()
+	
+};
